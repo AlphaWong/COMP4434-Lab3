c07652a34ead95fa1c10d5aac4d8bfbc8e8f9c5b
Alpha <jhexe2002@gmail.com>
2017-11-06 20:04:40 +0800

    ADD: Code.

--- Lab_3/code/Lab_3_DNN.py
+++ Lab_3/code/Lab_3_DNN.py
@@ -14,36 +14,36 @@ import pandas as pd
 
 ## Construct the training set
 ## Read the loan_status_training.csv as Train
-Train = 'Please fill in the missing code'
+Train = pd.read_csv('./Online_Courses_training.csv')
 ## Create a float32 array for Train.
-Train = 'Please fill in the missing code'
+Train = np.array(Train).astype(np.float32)
 ## Extract the training features as x_train
-x_train = 'Please fill in the missing code'
+x_train = Train[:, 1:]
 ## Extract the training labels as y_train
-y_train = 'Please fill in the missing code'
+y_train = Train[:, 0]
 
 ## Construct the testing set
 ## Read the loan_status_testing.csv as Test
-Test = 'Please fill in the missing code'
+Test = pd.read_csv('./Online_Courses_testing.csv')
 ## Create a float32 array for Test.
-Test = 'Please fill in the missing code'
+Test = np.array(Train).astype(np.float32)
 ## Extract the testing features as x_test
-x_test = 'Please fill in the missing code'
+x_test = Test[:,1:]
 ## Extract the testing labels as y_test
-y_test = 'Please fill in the missing code'
+y_test = Test[:,0]
 
 ## Specify that all features have real-value data
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension = len(Train[0]) - 1)]
 
 ## Build 3 layer DNN with 20, 20, 10 units respectively using tf.contrib.learn
-classifier = 'Please fill in the missing code'
+classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=[20,20,10], n_classes=2)
 
 ## Fit the DNN model using training data and labels
-'Please fill in the missing code'
+classifier.fit(x = x_train, y = y_train, steps = 3000)
 
 ## Prediction
 ## Use the test data and ground truth labels to test the classifier
-accuracy_score = 'Please fill in the missing code'
+accuracy_score = classifier.evaluate(x = x_test, y = y_test)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
 
 
